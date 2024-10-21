ea996aa878af014ea01d2aa1db8524c2cf255a98
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-10-21 00:55:37 -0300

    Fix: login com token

--- backend/usuarios/views.py
+++ backend/usuarios/views.py
@@ -1,12 +1,43 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action,permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 from .models import Usuario, Vaga
+from rest_framework.authtoken.models import Token
 from usuarios.serializers import UsuarioSerializer, VagaSerializer
 # Create your views here.
 
 class UsuarioViewSets(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
+
+
+    @action(detail=False, methods=['post'], url_path='login', url_name='login')
+    @permission_classes([AllowAny])
+    def login(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        if not username or not password:
+            return Response({"detail": "Por favor informe o usuário e senha"}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            usuario = Usuario.objects.get(username=username)
+
+            if usuario.check_password(password):  # Use o método check_password
+                # Gere o token de autenticação
+                token, created = Token.objects.get_or_create(user=usuario)
+                return Response({
+                    "detail": "Login feito com sucesso",
+                    "token": token.key
+                }, status=status.HTTP_200_OK)
+            else:
+                return Response({"detail": "Senha incorreta"}, status=status.HTTP_400_BAD_REQUEST)
+
+        except Usuario.DoesNotExist:
+            return Response({"detail": "Usuário não encontrado"}, status=status.HTTP_400_BAD_REQUEST)
+
+
 
 class VagaViewSets(viewsets.ModelViewSet):
     queryset = Vaga.objects.all()
